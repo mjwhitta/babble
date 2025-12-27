855a7d01f57231826c29a92ef07389becaac7f3d
Miles Whittaker <mj@whitta.dev>
2025-12-26 23:00:26 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -3,7 +3,7 @@ package babble
 import "regexp"
 
 // Version is the package version.
-const Version string = "0.4.3"
+const Version string = "0.4.4"
 
 var (
 	// CryptoSecure determines whether or not to use a
